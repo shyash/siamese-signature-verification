923fd3b1033cdf09c4050bd4c7a1e3ec4d062bd6
shyash <shyash222@gmail.com>
2020-02-05 23:48:41 +0530

    update

--- src/main/Model.py
+++ src/main/Model.py
@@ -8,15 +8,15 @@ model = tf.keras.models.Sequential(
         tf.keras.layers.MaxPooling2D(3, 3),
         tf.keras.layers.Conv2D(256, (5, 5), activation="relu"),
         tf.keras.layers.MaxPooling2D(3, 3),
-        tf.keras.layers.Dropout(0.01, noise_shape=None, seed=None),
+        tf.keras.layers.Dropout(0.3, noise_shape=None, seed=None),
         tf.keras.layers.Conv2D(384, (3, 3), activation="relu"),
         tf.keras.layers.Conv2D(256, (3, 3), activation="relu"),
         tf.keras.layers.MaxPooling2D(3, 3),
-        tf.keras.layers.Dropout(0.01, noise_shape=None, seed=None),
+        tf.keras.layers.Dropout(0.3, noise_shape=None, seed=None),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.01, noise_shape=None, seed=None),
-        tf.keras.layers.Dense(1, activation="relu"),
+        tf.keras.layers.Dropout(0.5, noise_shape=None, seed=None)
     ]
 )
+
 
